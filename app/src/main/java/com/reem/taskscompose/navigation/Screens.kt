1072b98eb3b83ea35cee2e087a08b1fcd7e72be6
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-08-25 01:32:04 +0200

    Update Screens.kt

--- app/src/main/java/com/reem/taskscompose/navigation/Screens.kt
+++ app/src/main/java/com/reem/taskscompose/navigation/Screens.kt
@@ -1,6 +1,10 @@
 package com.reem.taskscompose.navigation
 
 import androidx.navigation.NavHostController
+import com.reem.taskscompose.util.Action
 
 class Screens(navHostController: NavHostController) {
+    val list: (Action) -> Unit = { action ->
+        navHostController.navigate("list/${action.name}")
+    }
 }
